074b1047d93376f3a0c23a4594c2cfce915f34b4
krapalik01 <krapalik03@mail.ru>
2025-10-12 01:15:34 +0300

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,14 @@ import { store } from './store/store';
 import { MantineProvider } from '@mantine/core';
 import App from './App';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter  } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <MantineProvider>
-      <BrowserRouter>
+      <HashRouter >
         <App />
-      </BrowserRouter>
+      </HashRouter >
     </MantineProvider>
   </Provider>
 );
